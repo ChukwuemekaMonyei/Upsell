729b574f3425b8f68aa37155fa470d65159f3185
ChukwuemekaMonyei <raphaelmonyei24@gmail.com>
2024-05-17 13:25:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+window.document.getElementById('demo').innerHTML = window.innerWidth + "px";
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.Logo, .para, .consumer-img, .sub2, .comm-header, .communityhd, .level-header, #Footer', { origin: 'top' });
+
+ScrollReveal().reveal('.consumer, .abt-comm', { origin: 'bottom' });
+
+ScrollReveal().reveal('.run, .sun', { origin: 'left' });
+
+ScrollReveal().reveal('.ran, .son', { origin: 'right' });
