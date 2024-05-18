f90fcdd30e6131a33d521676dab61c457534fb45
ChukwuemekaMonyei <raphaelmonyei24@gmail.com>
2024-05-18 07:54:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,3 @@ ScrollReveal({
 ScrollReveal().reveal('.Logo, .para, .consumer-img, .sub2, .comm-header, .communityhd, .level-header, #Footer', { origin: 'top' });
 
 ScrollReveal().reveal('.consumer, .abt-comm', { origin: 'bottom' });
-
-//ScrollReveal().reveal('.run, .sun', { origin: 'left' });
-
-//ScrollReveal().reveal('.ran, .son', { origin: 'right' });
